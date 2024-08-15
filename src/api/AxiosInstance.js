bb52a5a712dd0a42f46e69374cf55fb4c6fa199e
Andika Risky Faizatama <andikafaizatama07@gmail.com>
2024-08-15 22:30:12 +0700

    set up backend

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Ganti dengan URL API yang sesuai
+  baseURL: 'http://test.msikaranganyar.com', // Ganti dengan URL API yang sesuai
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
 });
--- src/stores/useCustomerStore.js
+++ src/stores/useCustomerStore.js
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import {create} from 'zustand';
 
 const useCustomerStore = create((set) => ({
   selectedCustomerId: null,
